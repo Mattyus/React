8d8b38389381db61cb319cb7385bdda16258e9c1
Sallardon Mathieu <sallardonm@gmail.com>
2018-03-17 17:24:49 +0100

    Validation modal

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/bootstrap.min.css';
-import './css/index.css'
+import './css/index.css';
+import 'react-datepicker/dist/react-datepicker.css';
 import { Table, Pagination, Button, Modal, FormGroup, Glyphicon, ControlLabel, FormControl } from 'react-bootstrap';
 import { WithContext as ReactTags } from 'react-tag-input';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+import Moment from 'react-moment';
 import flickr from './img/flickr.jpg';
 import vimeo from './img/vimeo.png';
 
@@ -12,13 +16,14 @@ class BookmarkCollection extends React.Component{
 	constructor(props) {
 		super(props)
 		this.state = {
-			data:[
+			id_sequence: 3,// next id
+			data:[// tableau des bookmark
 				{
 					id:1,
 					url:'https://www.flickr.com/photos/75506839@N06/6940508877/in/photolist-bziVua-gyVf8L-4k5CZk-5iynax-gKydF2-uPg1t-9fSwAe-rPL67U-Co9A1Y-6C8UGH-eJEqt-sMMDA-2fvHr-22w7Hk-DZn86-5de5bB-9LJ5Rh-kp6ByD-8hr5oQ-8BeiQA-Bkvu8-5dTJuk-3TW6mx-8SK6ma-8cx1yH-mWuUV-66qyvc-7L94pZ-dRpZv7-6eT2HN-8wR9rb-4J8fQb-c2hEUN-5d8jmt-dPwY5t-4agFj4-5y1kHM-5JBPJY-5nBeH9-8spoWQ-dJFFbN-5s6zvd-pg6b9K-E18PX-5GrwNN-bDVap8-4HBaS1-7UXYSQ-gbNoMr-Mnufa',
 					title:'Ange',
 					author_name:'AbelleAnanta',
-					added_date:'28/01/2012',
+					added_date:moment(),
 					width:'350',
 					height:'323',
 					duration:'',
@@ -29,7 +34,7 @@ class BookmarkCollection extends React.Component{
 					url:'https://vimeo.com/52270684',
 					title:'Manga',
 					author_name:'mozuya',
-					added_date:'28/01/2012',
+					added_date:moment(),
 					width:'350',
 					height:'323',
 					duration:'00:34',
@@ -44,27 +49,21 @@ class BookmarkCollection extends React.Component{
 		
 	}
 	
-	findNewId(){
-		let id = 1;
-		while (1){
-			if (!this.state.data.find(x => x.id === id)){
-				return id;
-			}
-			id ++;
-		}
-	}
-	
+	// Ajout d'un bookmark
 	addBookmark(bookmark){
-		bookmark.id = this.findNewId();
+		bookmark.id = this.state.id_sequence;
 		let data = this.state.data.concat(bookmark);
 		this.setState({data: data});
+		this.setState({id_sequence: this.state.id_sequence + 1});
 	}
 	
+	// Suppression d'un bookmark
 	delBookmark(id){
 		let data = this.state.data.filter((e) => e.id !== id);
 		this.setState({data: data});
 	}
 	
+	// Modification d'un bookmark
 	modifBookmark(bookmark){
 		let data = this.state.data;
 		let item = data.find(x => x.id === bookmark.id);
@@ -89,7 +88,7 @@ class BookmarkCollection extends React.Component{
 
 /** Bookmark */
 class Bookmark extends React.Component{
-	render() {
+	render() {																						   
 		let image = null;
 		if (this.props.bookmark.url.startsWith('https://www.flickr.com')) {
 			image = <img alt="flickr" src={flickr}/>;
@@ -103,9 +102,9 @@ class Bookmark extends React.Component{
             	<td>{this.props.bookmark.url}</td>
             	<td>{this.props.bookmark.title}</td>
             	<td>{this.props.bookmark.author_name}</td>
-            	<td>{this.props.bookmark.added_date}</td>
-				<td><Button bsStyle="primary" onClick={() => this.props.modif(this.props.bookmark)}><Glyphicon glyph="pencil"/></Button></td>
-				<td><Button bsStyle="danger" onClick={() => this.props.del(this.props.bookmark.id)}><Glyphicon glyph="remove-circle"/></Button></td>
+            	<td><Moment format="DD/MM/YYYY">{this.props.bookmark.added_date}</Moment></td>
+				<td className="col-button"><Button bsStyle="primary" onClick={() => this.props.modif(this.props.bookmark)}><Glyphicon glyph="pencil"/></Button></td>
+				<td className="col-button"><Button bsStyle="danger" onClick={() => this.props.del(this.props.bookmark.id)}><Glyphicon glyph="remove-circle"/></Button></td>
 				
           	</tr>
 		);
@@ -248,6 +247,7 @@ class BookmarkList extends React.Component{
 /** BookmarkForm */
 class BookmarkForm extends React.Component{
 	constructor(props) {
+		moment.locale("fr");
 		super(props)
 		this.state = {
 			modal:false,
@@ -257,12 +257,19 @@ class BookmarkForm extends React.Component{
 				url:'',
 				title:'',
 				author_name:'',
-				added_date:'',
+				added_date:null,
 				width:'',
 				height:'',
 				duration:'',
 				tags:[]
-			}
+			},
+			urlHasError: false,
+			titleHasError: false,
+			authorHasError: false,
+			dateHasError: false,
+			widthHasError: false,
+			heightHasError: false,
+			durationHasError: false
 		}
 		this.toggle = this.toggle.bind(this);
 		this.handleUrlChange = this.handleUrlChange.bind(this);
@@ -273,10 +280,12 @@ class BookmarkForm extends React.Component{
 		this.handleHeightChange = this.handleHeightChange.bind(this);
 		this.handleTitleChange = this.handleTitleChange.bind(this);
 		this.handleDurationChange = this.handleDurationChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
 		this.handleDelete = this.handleDelete.bind(this);
 		this.handleAddition = this.handleAddition.bind(this);
 		this.handleDrag = this.handleDrag.bind(this);
+		this.validationForm = this.validationForm.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+		
 	}
 	
 	getInitialState() {		
@@ -319,9 +328,9 @@ class BookmarkForm extends React.Component{
 		this.setState({bookmark});
 	}
 	
-	handleAddedDateChange(e) {
+	handleAddedDateChange(date) {
 		let bookmark = {...this.state.bookmark};
-		bookmark.added_date = e.target.value;
+		bookmark.added_date = date;
 		this.setState({bookmark});
 	}
 	
@@ -369,18 +378,65 @@ class BookmarkForm extends React.Component{
         this.setState({bookmark});
     }
 	
+	validationForm(){
+		let formHasError = false;
+		if (this.state.bookmark.url.trim()){// validation url	
+			this.setState({urlHasError: false});
+		} else {
+			this.setState({urlHasError: true});
+			formHasError = true;
+		}
+		
+		if (this.state.bookmark.title.trim()){// validation title
+			this.setState({titleHasError: false});
+		} else {
+			this.setState({titleHasError: true});
+			formHasError = true;
+		}
+		
+		if (this.state.bookmark.author_name.trim()){// validation author_name
+			this.setState({authorHasError: false});
+		} else {
+			this.setState({authorHasError: true});
+			formHasError = true;
+		}
+		
+		if (this.state.bookmark.added_date){// validation added_date
+			this.setState({dateHasError: false});
+		} else {
+			this.setState({dateHasError: true});
+			formHasError = true;
+		}
+
+		if (this.state.bookmark.width.trim()){// validation width
+			this.setState({widthHasError: false});
+		} else {
+			this.setState({widthHasError: true});
+			formHasError = true;
+		}
+		
+		if (this.state.bookmark.height.trim()){// validation height
+			this.setState({heightHasError: false});
+		} else {
+			this.setState({heightHasError: true});
+			formHasError = true;
+		}
+
+		if (this.isVideo()){
+			if (this.state.bookmark.height.trim()){// validation duration
+				this.setState({durationHasError: false});
+			} else {
+				this.setState({durationHasError: true});
+				formHasError = true;
+			}
+		}
+
+		return formHasError;
+	}
+	
 	handleSubmit(e) {
 		e.preventDefault();
-		var url = this.state.bookmark.url.trim();
-		var title = this.state.bookmark.title.trim();
-		var author_name = this.state.bookmark.author_name.trim();
-		var added_date = this.state.bookmark.added_date.trim();
-		var width = this.state.bookmark.width.trim();
-		var height = this.state.bookmark.height.trim();
-		if (this.isVideo()){
-			var duration = this.state.bookmark.duration.trim();
-		}
-		if (!url || !title || !author_name || !added_date || !width || !height) {
+		if (this.validationForm()) {
 			return;
 		}
 		this.props.onBookmarkSubmit(this.state.bookmark);
@@ -405,34 +461,41 @@ class BookmarkForm extends React.Component{
 					<form className="bookmarkForm" onSubmit={this.handleSubmit}>
 						<Modal.Header closeButton>{this.props.title}</Modal.Header>
 						<Modal.Body>
-							<FormGroup>
+							<FormGroup className={this.state.urlHasError?'has-error':''}>
 								<ControlLabel>Url</ControlLabel>
-								<FormControl type="text" name="url" id ="url" onChange={this.handleUrlChange} value={this.state.bookmark.url || ''} required/>
+								<FormControl type="text" name="url" id ="url" onChange={this.handleUrlChange} value={this.state.bookmark.url || ''}/>
+								{this.state.urlHasError?<p className="text-danger">Le champ Url est obligatoire</p> : ''}								
 							</FormGroup>
-							<FormGroup>
+							<FormGroup className={this.state.titleHasError?'has-error':''}>
 								<ControlLabel>Title</ControlLabel>
-								<FormControl type="text" name="title" id="title" onChange={this.handleTitleChange} value={this.state.bookmark.title || ''} required/>
+								<FormControl type="text" name="title" id="title" onChange={this.handleTitleChange} value={this.state.bookmark.title || ''}/>
+								{this.state.titleHasError?<p className="text-danger">Le champ Title est obligatoire</p> : ''}	
 							</FormGroup>
-							<FormGroup>
+							<FormGroup className={this.state.authorHasError?'has-error':''}>
 								<ControlLabel>Author Name</ControlLabel>
-								<FormControl type="text" name="author_name" id="author_name" onChange={this.handleAuthorNameChange} value={this.state.bookmark.author_name || ''} required/>
+								<FormControl type="text" name="author_name" id="author_name" onChange={this.handleAuthorNameChange} value={this.state.bookmark.author_name || ''}/>
+								{this.state.authorHasError?<p className="text-danger">Le champ Author Name est obligatoire</p> : ''}
 							</FormGroup>
-							<FormGroup>
+							<FormGroup className={this.state.dateHasError?'has-error':''}>
 								<ControlLabel>Added Date</ControlLabel>
-								<FormControl type="text" name="added_date" id="added_date" onChange={this.handleAddedDateChange} value={this.state.bookmark.added_date || ''} required/>
+								<DatePicker id="added_date" name="added_date" className="form-control"  selected={this.state.bookmark.added_date} onChange={this.handleAddedDateChange}  dateFormat="DD/MM/YYYY"/>
+								{this.state.dateHasError?<p className="text-danger">Le champ Added Date est obligatoire</p> : ''}
 							</FormGroup>
-							<FormGroup>
+							<FormGroup className={this.state.widthHasError?'has-error':''}>
 								<ControlLabel>Width</ControlLabel>
-								<FormControl type="text" name="width" id="width" onChange={this.handleWidthChange} value={this.state.bookmark.width || ''} required/>
+								<FormControl type="text" name="width" id="width" onChange={this.handleWidthChange} value={this.state.bookmark.width || ''}/>
+								{this.state.widthHasError?<p className="text-danger">Le champ Width est obligatoire</p> : ''}
 							</FormGroup>
-							<FormGroup>
+							<FormGroup className={this.state.heightHasError?'has-error':''}>
 								<ControlLabel>Height</ControlLabel>
-								<FormControl type="text" name="height" id="height" onChange={this.handleHeightChange} value={this.state.bookmark.height || ''} required/>
+								<FormControl type="text" name="height" id="height" onChange={this.handleHeightChange} value={this.state.bookmark.height || ''}/>
+								{this.state.heightHasError?<p className="text-danger">Le champ Height est obligatoire</p> : ''}
 							</FormGroup>
 							{this.state.isVideo?
-								<FormGroup>
+								<FormGroup className={this.state.durationHasError?'has-error':''}>
 									<ControlLabel>Duration</ControlLabel>
-									<FormControl type="text" name="duration" id="duration" onChange={this.handleDurationChange} value={this.state.bookmark.duration || ''} required/>
+									<FormControl type="text" name="duration" id="duration" onChange={this.handleDurationChange} value={this.state.bookmark.duration || ''}/>
+									{this.state.durationHasError?<p className="text-danger">Le champ Duration est obligatoire</p> : ''}
 								</FormGroup>
 							:null}
 							<FormGroup>
